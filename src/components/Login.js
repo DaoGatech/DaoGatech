9e62e2cbf7ced58e59e5c466301b7b7bd7b7f70d
DaoGatech <stephenhdao@gatech.edu>
2016-11-26 23:31:02 -0500

    fix em problem

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import {OAuthSignInButton} from "redux-auth/material-ui-theme";
+import {Button} from 'react-bootstrap';
+import Logo from '../assets/logo.png';
 
 class Login extends Component {
-  
-  
-
   render() {
     return (  
-      <OAuthSignInButton provider="github" />
+      <div className="App">
+        <div className="App-header">
+          <div className="nav">
+            <div className="left-section">
+              <img src={Logo} className="App-logo" alt="logo" /> 
+              <div className="app-title">DAOGATECH</div>
+            </div>
+            <div className="right-section">         
+              <button><a style={{"textDecoration": "none", "color": "inherit"}} href="/photos">Photos</a></button>
+              <button><a style={{"textDecoration": "none", "color": "inherit"}} href="/">Home</a></button>
+            </div>
+            
+          </div>
+        </div>
+        <Button bsStyle="primary">Sign In</Button>
+      </div>
+
     );
   }
  
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Photos from './components/Photos';
+import Login from './components/Login';
 import './css/index.css';
 import {Router, Route, browserHistory, Link} from 'react-router';
 
@@ -9,6 +10,7 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}/>
     <Route path="/photos" component={Photos}/>
+    <Route path="/login" component={Login}/>
   </Router>, 
   document.getElementById('root')
 );
