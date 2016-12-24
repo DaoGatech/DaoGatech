ba1b15d83cee3b7c30389fbf29568ba9ecdb0018
DaoGatech <stephenhdao@gatech.edu>
2016-12-24 10:26:37 -0700

    add lock to admin page

--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ import App from './components/App';
 import Photos from './components/Photos';
 import Login from './components/Login';
 import Admin from './components/Admin';
+import AuthService from './utils/AuthService'
 import './css/index.css';
 import {Router, Route, browserHistory, Link} from 'react-router';
 
+const auth = new AuthService('MD0gkfSGRtjwOyshSOQHxFWDlSe3sLzC', 'tmizzle2005.auth0.com');
+
+// validate authentication for private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' })
+  }
+}
+
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={browserHistory} auth={auth}>
     <Route path="/" component={App}/>
     <Route path="/photos" component={Photos}/>
     <Route path="/login" component={Login}/>
-    <Route path="/admin" component={Admin}/>
+    <Route path="/admin" component={Admin} onEnter={requireAuth}/>
   </Router>, 
   document.getElementById('root')
 );
